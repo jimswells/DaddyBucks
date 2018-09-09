c4c34d0709de848d981a6333faefa58551f039a6
Jim Wells <Jim.s.wells@disney.com>
2018-09-09 16:58:24 -0400

    google oauth is silly

--- scripts/account.js
+++ scripts/account.js
@@ -126,3 +126,46 @@ function deductFunds() {
 
 
 }
+
+/**
+         * Sample JavaScript code for sheets.spreadsheets.values.append
+         * See instructions for running APIs Explorer code samples locally:
+         * https://developers.google.com/explorer-help/guides/code_samples#javascript
+         */
+      
+        function authenticate() {
+            return gapi.auth2.getAuthInstance()
+                .signIn({scope: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/spreadsheets"})
+                .then(function() { console.log("Sign-in successful"); },
+                      function(err) { console.error("Error signing in", err); });
+          }
+          function loadClient() {
+            return gapi.client.load("https://content.googleapis.com/discovery/v1/apis/sheets/v4/rest")
+                .then(function() { console.log("GAPI client loaded for API"); },
+                      function(err) { console.error("Error loading GAPI client for API", err); });
+          }
+          // Make sure the client is loaded and sign-in is complete before calling this method.
+          function execute() {
+            return gapi.client.sheets.spreadsheets.values.append({
+              "spreadsheetId": "14G5JY_DGl4mXpTFOyIZejgTnaSRhhaObpJH5N7HtzIs",
+              "range": getAccountID() + "D:E",
+              "valueInputOption": "RAW",
+              "resource": {
+                "values": [
+                  [
+                    '09/09/2018',
+                    'Test3',
+                    15
+                  ]
+                ]
+              }
+            })
+                .then(function(response) {
+                        // Handle the results here (response.result has the parsed body).
+                        console.log("Response", response);
+                      },
+                      function(err) { console.error("Execute error", err); });
+          }
+          gapi.load("client:auth2", function() {
+            gapi.auth2.init({client_id: '767773074311-g2vffu719ldvp3jh2ktjfipqqfcgqq4d.apps.googleusercontent.com'});
+          });
